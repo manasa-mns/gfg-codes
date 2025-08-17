b54f6b7b1cbaa5096ebf7f27e8c0b26377524e21
manasa-mns <157282272+manasa-mns@users.noreply.github.com>
2025-08-17 19:03:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort by Absolute Difference/sort-by-absolute-difference.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void rearrange(int[] arr, int x) {
+        // code here
+          // code here
+        ArrayList<Integer>ar=new ArrayList<>();
+        for(int i=0;i<arr.length;i++)
+        ar.add(arr[i]);
+        Collections.sort(ar,(a,b)->{
+            return Math.abs(a-x)-Math.abs(b-x);
+        });
+        for(int i=0;i<arr.length;i++)
+        arr[i]=ar.get(i);
+    }
+}
