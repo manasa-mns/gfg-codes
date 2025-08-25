2571f5b5c0aae1dd3503f56a51cbc20cf33879ed
manasa-mns <157282272+manasa-mns@users.noreply.github.com>
2025-08-25 10:20:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Identical Trees/identical-trees.java
@@ -0,0 +1,21 @@
+/*
+class Node{
+    int data;
+    Node left,right;
+    Node(int d){
+        data=d;
+        left=right=null;
+    }
+}*/
+
+class Solution {
+    boolean isIdentical(Node root1, Node root2) {
+        // code here
+        if(root1==null && root2==null)
+        return true;
+        if(root1==null || root2==null)
+        return false;
+        return (root1.data==root2.data) && isIdentical(root1.left,root2.left)
+        && isIdentical(root1.right,root2.right);
+    }
+}
