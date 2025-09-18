b617e6dc9a2432357a6ee29bf388d53f1baaf1bd
manasa-mns <157282272+manasa-mns@users.noreply.github.com>
2025-09-18 15:28:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum of dependencies in a graph/sum-of-dependencies-in-a-graph.java
@@ -0,0 +1,9 @@
+//Back-end complete function Template for Java
+
+class Solution {
+    // function to calculate the sum of dependencies in the graph
+    int sumOfDependencies(int V, int[][] edges) {
+        // code here
+        return edges.length;
+    }
+};
